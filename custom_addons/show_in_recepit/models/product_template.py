5e8d6cb58a6600c62678ccdc8a5660663609838c
Ahmed Abdin <ahmed.g.abdien@gmail.com>
2022-09-16 07:00:35 +0300

    add showInReceipt view

---
+++ custom_addons/show_in_recepit/models/product_template.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+
+class ProductTemplate(models.Model):
+    _inherit = 'product.template'
+
+    showInReceipt = fields.Boolean(
+        string='showInReceipt',
+    )

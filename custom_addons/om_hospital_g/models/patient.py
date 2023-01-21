d63242b3f93bb6ee5f4e3d56304160a7e1b1411b
Ahmed Abdin <ahmed.g.abdien@gmail.com>
2023-01-21 18:38:26 +0300

    Recap: 16. Adding chatter

--- custom_addons/om_hospital_g/models/patient.py
+++ custom_addons/om_hospital_g/models/patient.py
@@ -4,6 +4,7 @@ from odoo import fields, models
 class HospitalPatient(models.Model):
     # that will create a table with name "hospital_patient"
     _name = "hospital.patientg"
+    _inherit = ['mail.thread', 'mail.activity.mixin']
     _description = "Hospital Patient G"
 
     active = fields.Boolean(string="Active", default=True)

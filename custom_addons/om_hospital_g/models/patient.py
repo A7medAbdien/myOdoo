c38bcf5a74023977895ac8f65afdc6b5344b24db
Ahmed Abdin <ahmed.g.abdien@gmail.com>
2023-01-22 19:15:12 +0300

    Recap: 24. Add Computed Field, age

--- custom_addons/om_hospital_g/models/patient.py
+++ custom_addons/om_hospital_g/models/patient.py
@@ -1,4 +1,6 @@
-from odoo import fields, models
+import datetime
+
+from odoo import fields, models, api
 
 
 class HospitalPatient(models.Model):
@@ -8,9 +10,23 @@ class HospitalPatient(models.Model):
     _description = "Hospital Patient G"
 
     active = fields.Boolean(string="Active", default=True)
-    age = fields.Integer(string="Age", tracking=True)
+    # age = fields.Integer(string="Age", tracking=True)
     # list with tubules (key,value)
     gender = fields.Selection(
         [('male', 'Male'), ('female', 'Female')], string="Gender", tracking=True)
     name = fields.Char(string="Name", tracking=True)
     ref = fields.Char(string="Recreance")
+
+    dob = fields.Date(string="Date of Birth")
+    # age = fields.Integer(string="Age", tracking=True, compute='_compute_age')
+    age = fields.Integer(string="Age", tracking=True, compute='_compute_age', store=True)
+
+    @api.depends('dob')
+    def _compute_age(self):
+        print("self................", self)
+        for rec in self:
+            today = datetime.date.today()
+            if rec.dob:
+                rec.age = today.year - rec.dob.year
+            else:
+                rec.age = 1

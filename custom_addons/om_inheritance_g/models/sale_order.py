1b9aee564ec7b43364f8974021764878b88b520b
Ahmed Abdin <ahmed.g.abdien@gmail.com>
2023-01-27 17:15:24 +0300

    Recap: 70. inherit a function

--- custom_addons/om_inheritance_g/models/sale_order.py
+++ custom_addons/om_inheritance_g/models/sale_order.py
@@ -1,9 +1,15 @@
-from odoo import models, fields, api
+from odoo import models, fields
+
 
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
     confirmed_user_id = fields.Many2one(
-    string='Confirmed User',
-    comodel_name='res.users',
+        string='Confirmed User',
+        comodel_name='res.users',
     )
+
+    def action_confirm(self):
+        super(SaleOrder, self).action_confirm()
+        print('Hi, I am working!! ..................')
+        self.confirmed_user_id = self.env.user.id
